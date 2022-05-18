c8e557985c0e0b25726fc2acea5891fe6433ea87
shirley <shirley41825@gmail.com>
2022-05-19 00:43:13 +0800

    install

---
+++ cms/install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install build-essential fpc postgresql postgresql-client gettext python2.7 python-setuptools python-tornado python-psycopg2 python-sqlalchemy python-psutil python-netifaces python-crypto python-tz python-six iso-codes shared-mime-info stl-manual python-beautifulsoup python-mechanize python-coverage python-mock cgroup-lite python-requests python-werkzeug python-gevent patool -y
+
+sudo apt-get install nginx-full php5-cli php5-fpm phppgadmin python-yaml python-sphinx texlive-latex-base python-cups a2ps -y
+
