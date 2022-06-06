9ffc653b22fd2b5295ffe3b3496eabf70ac2f9ef
shirley <shirley41825@gmail.com>
2022-06-06 22:58:08 +0800

    add file

---
+++ install2.sh
@@ -0,0 +1,11 @@
+sudo -u postgres createuser cmsuser -P
+
+sudo -u postgres createdb -O cmsuser database
+
+sudo -u postgres psql database -c 'ALTER SCHEMA public OWNER TO cmsuser'
+
+sudo -u postgres psql database -c 'GRANT SELECT ON pg_largeobject TO cmsuser'
+
+sudo -u postgres psql database -c "INSERT INTO admins (adminname,password,identity) VALUES ('admin', 'admin',1);"
+
+exit
