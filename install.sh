08a5652d2471e76c70a0de200fbd0593f7eed73c
shirley <shirley41825@gmail.com>
2022-06-06 21:39:09 +0800

    add file

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install build-essential fpc postgresql postgresql-client gettext python2.7 python-setuptools python-tornado python-psycopg2 python-sqlalchemy python-psutil python-netifaces python-crypto python-tz python-six iso-codes shared-mime-info stl-manual python-beautifulsoup python-mechanize python-coverage python-mock cgroup-lite python-requests python-werkzeug python-gevent patool -y
 
-cd cms
+sudo chmod 755 setup.py
 
 ./setup.py build
 
@@ -8,7 +8,5 @@ sudo ./setup.py install
 
 sudo usermod -a -G cmsuser shirley
 
-exit
-
-sudo su - postgres
+shutdown -r 0S
 
