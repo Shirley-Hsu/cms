32c7bd6caedf76826167ed3d434c81dc0d478636
shirley <shirley41825@gmail.com>
2022-06-06 21:06:20 +0800

    add file

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 sudo apt-get install build-essential fpc postgresql postgresql-client gettext python2.7 python-setuptools python-tornado python-psycopg2 python-sqlalchemy python-psutil python-netifaces python-crypto python-tz python-six iso-codes shared-mime-info stl-manual python-beautifulsoup python-mechanize python-coverage python-mock cgroup-lite python-requests python-werkzeug python-gevent patool -y
 
-sudo apt-get install nginx-full php5-cli php5-fpm phppgadmin python-yaml python-sphinx texlive-latex-base python-cups a2ps -y
+cd cms
 
-sudo apt-get install git -y
+./setup.py build
 
-git clone https://github.com/Shirley-Hsu/cms.git
+sudo ./setup.py install
+
+sudo usermod -a -G cmsuser shirley
+
+exit
+
+sudo su - postgres
 
