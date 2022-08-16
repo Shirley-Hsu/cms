943c9a3dc6257716bc93bd286dcddd9f7702a365
shirley <shirley41825@gmail.com>
2022-08-16 21:46:25 +0800

    add file

--- install3.sh
+++ install3.sh
@@ -3,3 +3,7 @@
 cmsInitDB
 
 sudo -u postgres psql database -c "INSERT INTO admins (adminname,password,identity) VALUES ('admin', 'admin',1);"
+
+sudo apt-get update
+sudo apt-get install openssh-server
+sudo service ssh start
